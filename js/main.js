67be555f17ffe579f1c76f28572a54f621963609
Madeleine Marcano <madeleine1302@gmail.com>
2019-03-04 17:04:40 -0300

    modales y botones

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function ocultarBtn1() {
+    document.getElementById("btn-webpay").innerHTML = "Webpay";
+}
+
+function ocultarBtn2() {
+    document.getElementById("btn-botones").innerHTML = "Botones Webpay";
+}
+
+function ocultarBtn3() {
+    document.getElementById("btn-plus").innerHTML = "Webpay Plus";
+}
+
+function ocultarBtn4() {
+    document.getElementById("btn-onepay").innerHTML = "Onepay";
+}
+
+
+// $(document).ready(function(){
+//     $("#btn1").click(function(){
+//       $("#change").append(" <b>Appended text</b>.");
+//     });
+//   });
